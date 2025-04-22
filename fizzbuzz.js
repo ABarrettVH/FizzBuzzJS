1d546beea1e5170e6bccbc5f85471aa9205c6a60
Alice Barrett <alice.barrett@softwire.com>
2025-04-22 14:33:53 +0100

    prints Fizz for multiples of 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,11 @@
 function fizzbuzz(){
     // console.log("Hello World");
     for (let i=1; i<101; i++){
+        if (i%3 == 0){
+            console.log("Fizz");
+        } else {
         console.log(i);
+        }
     }
     
 }
